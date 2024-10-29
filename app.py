b28d2fd0355a234e546075130dc8a333f66c8200
Mehmet Akif Gültekin <mehmetag@uia.no>
2024-10-29 12:16:11 +0100

    added auth_template

--- app.py
+++ app.py
@@ -1,15 +1,56 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
+import bcrypt
 import bleach
+import pyotp
+from flask import Flask, request, redirect, url_for, render_template, session, flash
+from datetime import datetime, timedelta
+import os
+
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+@app.route('/qr_code')
+def qr_code():
+    return render_template('qr_code.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    if request.method == 'POST':
+        email = bleach.clean(request.form['email'])
+        password = bleach.clean(request.form['password'])
+        confirm_password = bleach.clean(request.form['confirm_password'])
+        error = None
+
+        if not email:
+            error = 'email is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif not confirm_password:
+            error = 'Confirm password is required.'
+        elif password != confirm_password:
+            error = 'Passwords do not match.'
+
+        if error is not None:
+            flash(error)
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO users (email, password_hash) VALUES (?, ?)',
+                         (email, password))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('qr_code'))
+
+    return render_template('register.html')
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
